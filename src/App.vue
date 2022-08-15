d47258db6fb67e22a2efd274b43de3b61fc0cb57
J3m1th <denys.petryniak@gmail.com>
2022-08-15 18:44:18 +0300

    Install vue-router & add home page

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <h1>Vue Voxer</h1>
       <p>Realtime Voice Chat</p>
     </header>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueRouter from 'vue-router';
 import { firestorePlugin } from 'vuefire';
+import Home from '@/components/Home';
 
+Vue.use(VueRouter);
 Vue.use(firestorePlugin);
 
 Vue.config.productionTip = false;
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount('#app');
