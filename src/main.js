98fa839bbbde1112349f155a79ae02f30f635820
J3m1th <denys.petryniak@gmail.com>
2022-08-16 18:43:46 +0300

    Implement dynamic routing

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import VueRouter from 'vue-router';
 import { firestorePlugin } from 'vuefire';
 import Home from '@/components/Home';
+import ChatRoom from '@/components/ChatRoom';
 
 Vue.use(VueRouter);
 Vue.use(firestorePlugin);
@@ -14,6 +15,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/chat/:id',
+    name: 'chat',
+    component: ChatRoom,
   },
 ];
 
