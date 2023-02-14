bc1eb7010c1793bcdd39815bc6d5adee83ecf216
Denys Petryniak <denys.petryniak@gmail.com>
2023-02-14 04:21:24 +0200

    Install bulma via npm & change global font to Nunito

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { VueFire, VueFireAuth } from 'vuefire';
 import { firebaseApp } from '@/firebase';
 import router from './router';
 import App from '@/App.vue';
+import '@/assets/styles/main.scss';
 
 const pinia = createPinia();
 const app = createApp(App);
