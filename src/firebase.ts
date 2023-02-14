c883d14587241ad9b1cd1a186273aa408b9cce5f
Denys Petryniak <denys.petryniak@gmail.com>
2023-02-14 06:36:48 +0200

    Refactoring

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,15 +8,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
-
-const firebaseConfig = {
-  appId: import.meta.env.VITE_APP_ID,
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_API_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-};
+import { firebaseConfig } from '@/firebaseConfig.js';
 
 export const firebaseApp = initializeApp(firebaseConfig);
 
